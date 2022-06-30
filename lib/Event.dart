e0f478c3a0a40a621ecb3a1368acb3a19eb30306
Filip <filip@filip.pl>
2022-06-30 13:39:40 +0200

    added event obj

---
+++ lib/Event.dart
@@ -0,0 +1,11 @@
+class Event{
+  String cardTitle;
+  String startDate;
+  String endDate;
+  String month;
+  String place;
+  String title;
+
+  Event(this.cardTitle, this.startDate, this.endDate, this.month, this.place, this.title);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:html/dom.dart' as dom;
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:web_scrap/Event.dart';
 
 import 'myAppBar.dart';
 
@@ -16,7 +17,7 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  List<String> eventList = [];
+  List<Event> eventList = [];
   bool isLoading = true;
 
   void getData() async {
@@ -24,12 +25,23 @@ class _MyWidgetState extends State<MyWidget> {
       Uri.parse('https://www.formula1.com/en/racing/2022.html'),
     );
 
-    dom.Document html = dom.Document.html( utf8.decode(latin1.encode(response.body), allowMalformed: true));
+    dom.Document html = dom.Document.html(
+        utf8.decode(latin1.encode(response.body), allowMalformed: true));
 
-    var eventList_child = html.getElementsByClassName('event-list')[0].children[0].children[0].children;
+    var eventList_child = html
+        .getElementsByClassName('event-list')[0]
+        .children[0]
+        .children[0]
+        .children;
 
     for (int i = 1; i < eventList_child.length; i++) {
-      eventList.add(eventList_child[i].getElementsByClassName('event-title')[0].text);
+      eventList.add(Event(
+          eventList_child[i].getElementsByClassName('card-title')[0].text,
+          eventList_child[i].getElementsByClassName('start-date')[0].text,
+          eventList_child[i].getElementsByClassName('end-date')[0].text,
+          eventList_child[i].getElementsByClassName('month-wrapper')[0].text,
+          eventList_child[i].getElementsByClassName('event-place')[0].text,
+          eventList_child[i].getElementsByClassName('event-title')[0].text));
     }
 
     setState(() {
@@ -44,21 +56,22 @@ class _MyWidgetState extends State<MyWidget> {
     }
     return MaterialApp(
       home: Scaffold(
-          appBar: MyAppBar(),
-          body: isLoading
-              ? CircularProgressIndicator()
-              : ListView.separated(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: eventList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 50,
-                      child: Center(child: Text(eventList[index])),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                )),
+        appBar: MyAppBar(),
+        body: isLoading
+            ? const CircularProgressIndicator()
+            : ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: eventList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    child: Center(child: Text(eventList[index].title)),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ),
+      ),
     );
   }
 }
