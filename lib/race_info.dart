209391bb650ac933652140d64ce9bad0b853d65f
Filip <filip.zmyslony@o2.pl>
2023-01-09 13:06:28 +0100

    bar fix

--- lib/race_info.dart
+++ lib/race_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:web_scrap/Event.dart';
 import 'package:web_scrap/myAppBar.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -15,8 +16,25 @@ class raceInfo extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: const MyAppBar(),
-        body: Center(
+        appBar: AppBar(
+          title: Text(
+            'F1 schedule',
+            style: GoogleFonts.montserrat(
+            fontWeight: FontWeight.w900,
+            fontSize: 25,
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 255, 6, 0),
+          centerTitle: true,
+          leading: IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    iconSize: 20.0,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },    
+          ),
+        ),
+          body: Center(
           child: ElevatedButton(
             style: style,
             onPressed: () => _launchURL(event.href),
@@ -32,7 +50,7 @@ _launchURL(String urlcd) async {
   var url = 'https://www.formula1.com$urlcd';
   final uri = Uri.parse(url);
   if (await canLaunchUrl(uri)) {
-    await launchUrl(uri);
+    await launchUrl(uri, mode: LaunchMode.inAppWebView);
   } else {
     throw 'Could not launch $url';
   }
